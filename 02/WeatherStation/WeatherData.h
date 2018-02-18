15c1120a7b8c7ac13c2bca1a53498b463c8e29fc
ML <lm1458777@gmail.com>
2018-02-18 13:40:58 +0300

    Выполнено задание №1 "Отображение полной статистической информации".

--- 02/WeatherStation/WeatherData.h
+++ 02/WeatherStation/WeatherData.h
@@ -1,9 +1,9 @@
 ﻿#pragma once
-#include <iostream>
-#include <vector>
+#include "Observer.h"
 #include <algorithm>
 #include <climits>
-#include "Observer.h"
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -14,7 +14,7 @@ struct SWeatherInfo
 	double pressure = 0;
 };
 
-class CDisplay: public IObserver<SWeatherInfo>
+class CDisplay : public IObserver<SWeatherInfo>
 {
 private:
 	/* Метод Update сделан приватным, чтобы ограничить возможность его вызова напрямую
@@ -30,6 +30,53 @@ private:
 	}
 };
 
+class StatsData
+{
+public:
+	using Value = double;
+
+	void Accumulate(Value newValue)
+	{
+		if (m_countAcc == std::numeric_limits<decltype(m_countAcc)>::max())
+		{
+			throw std::runtime_error("Too many data");
+		}
+
+		m_min = std::min(m_min, newValue);
+
+		m_max = std::max(m_max, newValue);
+
+		m_acc += newValue;
+		++m_countAcc;
+	}
+
+	Value GetMinValue() const
+	{
+		return m_min;
+	}
+
+	Value GetMaxValue() const
+	{
+		return m_max;
+	}
+
+	Value GetAverageValue() const
+	{
+		return !HasData() ? 0 : (m_acc / m_countAcc);
+	}
+
+	bool HasData() const
+	{
+		return m_countAcc > 0;
+	}
+
+private:
+	Value m_min = std::numeric_limits<double>::infinity();
+	Value m_max = -std::numeric_limits<double>::infinity();
+	Value m_acc = 0;
+	unsigned m_countAcc = 0;
+};
+
 class CStatsDisplay : public IObserver<SWeatherInfo>
 {
 private:
@@ -39,45 +86,45 @@ private:
 	*/
 	void Update(SWeatherInfo const& data) override
 	{
-		if (m_minTemperature > data.temperature)
-		{
-			m_minTemperature = data.temperature;
-		}
-		if (m_maxTemperature < data.temperature)
-		{
-			m_maxTemperature = data.temperature;
-		}
-		m_accTemperature += data.temperature;
-		++m_countAcc;
+		m_temperature.Accumulate(data.temperature);
+		m_humidity.Accumulate(data.humidity);
+		m_pressure.Accumulate(data.pressure);
 
-		std::cout << "Max Temp " << m_maxTemperature << std::endl;
-		std::cout << "Min Temp " << m_minTemperature << std::endl;
-		std::cout << "Average Temp " << (m_accTemperature / m_countAcc) << std::endl;
+		Display(std::cout, "Temp    ", m_temperature);
+		Display(std::cout, "Humidity", m_humidity);
+		Display(std::cout, "Pressure", m_pressure);
 		std::cout << "----------------" << std::endl;
 	}
 
-	double m_minTemperature = std::numeric_limits<double>::infinity();
-	double m_maxTemperature = -std::numeric_limits<double>::infinity();
-	double m_accTemperature = 0;
-	unsigned m_countAcc = 0;
+	static void Display(std::ostream& out, const char* name, const StatsData& data)
+	{
+		out << name << ": "
+			<< "Average = " << data.GetAverageValue()
+			<< "; Min = " << data.GetMinValue()
+			<< "; Max = " << data.GetMaxValue()
+			<< "." << std::endl;
+	}
 
+	StatsData m_temperature;
+	StatsData m_humidity;
+	StatsData m_pressure;
 };
 
 class CWeatherData : public CObservable<SWeatherInfo>
 {
 public:
 	// Температура в градусах Цельсия
-	double GetTemperature()const
+	double GetTemperature() const
 	{
 		return m_temperature;
 	}
 	// Относительная влажность (0...100)
-	double GetHumidity()const
+	double GetHumidity() const
 	{
 		return m_humidity;
 	}
 	// Атмосферное давление (в мм.рт.ст)
-	double GetPressure()const
+	double GetPressure() const
 	{
 		return m_pressure;
 	}
@@ -95,8 +142,9 @@ public:
 
 		MeasurementsChanged();
 	}
+
 protected:
-	SWeatherInfo GetChangedData()const override
+	SWeatherInfo GetChangedData() const override
 	{
 		SWeatherInfo info;
 		info.temperature = GetTemperature();
@@ -104,8 +152,9 @@ protected:
 		info.pressure = GetPressure();
 		return info;
 	}
+
 private:
 	double m_temperature = 0.0;
-	double m_humidity = 0.0;	
-	double m_pressure = 760.0;	
+	double m_humidity = 0.0;
+	double m_pressure = 760.0;
 };
