7095055a5b24fa6f7b59a0c200fa8af4416cec46
Maksim Levakov <maksim.levakov@ispringsolutions.com>
2018-03-30 12:30:14 +0300

    Вместо std::generate() используется std::iota().

--- 03/libstreams/Cypher.cpp
+++ 03/libstreams/Cypher.cpp
@@ -6,10 +6,7 @@ Cypher GenerateCypher(unsigned seed)
 	using namespace std;
 
 	Cypher cypher;
-	uint8_t i = 0;
-	generate(cypher.begin(), cypher.end(), [&i]() {
-		return i++;
-	});
+	iota(cypher.begin(), cypher.end(), 0);
 
 	shuffle(cypher.begin(), cypher.end(), std::mt19937(seed));
 
