5dc0bb371b3afbbffffad51690710c3888d81f8a
Maksim Levakov <maksim.levakov@ispringsolutions.com>
2018-05-24 19:07:15 +0300

    Добавлены CreateNoFillStyle() и CreateNoLineStyle().

--- 07/CompositeLib/StyleFactory.h
+++ 07/CompositeLib/StyleFactory.h
@@ -1,7 +1,9 @@
 #pragma once
-#include "IStyle_fwd.h"
 #include "CommonTypes.h"
+#include "IStyle_fwd.h"
 
 IFillStylePtr CreateFillStyle(RGBAColor color, bool enable = true);
-ILineStylePtr CreateLineStyle(RGBAColor color, float width = 1.f, bool enable = true);
+IFillStylePtr CreateNoFillStyle();
 
+ILineStylePtr CreateLineStyle(RGBAColor color, float width = 1.f, bool enable = true);
+ILineStylePtr CreateNoLineStyle();
