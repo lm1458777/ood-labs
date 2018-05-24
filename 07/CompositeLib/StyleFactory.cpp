4225451cae7ccc3adbe58175cf2196b7484da571
Maksim Levakov <maksim.levakov@ispringsolutions.com>
2018-05-24 19:07:15 +0300

    Добавлены CreateNoFillStyle() и CreateNoLineStyle().

--- 07/CompositeLib/StyleFactory.cpp
+++ 07/CompositeLib/StyleFactory.cpp
@@ -7,7 +7,17 @@ IFillStylePtr CreateFillStyle(RGBAColor color, bool enable)
 	return std::make_shared<FillStyle>(color, enable);
 }
 
+IFillStylePtr CreateNoFillStyle()
+{
+	return CreateFillStyle(0, false);
+}
+
 ILineStylePtr CreateLineStyle(RGBAColor color, float width, bool enable)
 {
 	return std::make_shared<LineStyle>(color, width, enable);
+}
+
+ILineStylePtr CreateNoLineStyle()
+{
+	return CreateLineStyle(0, 0, false);
 }
